fc7bbf7ab2e2e14ae3b57d65bbabc7f49d81883a
hamidrajabi <hamid1rajabi@gmail.com>
2022-03-01 12:52:39 +0330

    Configure static url

--- Main/urls.py
+++ Main/urls.py
@@ -8,9 +8,13 @@ from . import views
 
 urlpatterns=[
 	#base url
+    re_path(r'^images/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+
 	path('',views.home,name="Home"),
 	path('single-work/<str:workTitle>/',views.single_work,name='single-work')
 	# path('order/',views.orderPage,name="Order"),
 	]
 
-urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
